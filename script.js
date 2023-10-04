1306f538b3400c1f5f953df6eb0a132881cc1860
KaizerBalite <146916590+KaizerBalite@users.noreply.github.com>
2023-10-04 14:37:39 +0800

    feat: Activity 2 in DSA
    Comparison Operators

---
+++ script.js
@@ -0,0 +1,13 @@
+//Activity 2
+
+let firstFavNum = 9;
+let secondFavNum = 0;
+
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum != secondFavNum);
